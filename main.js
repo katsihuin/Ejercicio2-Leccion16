f6aacfe7fb3b5749ef53a8f71eda4d77291038fb
katsihuin <katsihuin@gmail.com>
2016-12-19 22:33:37 -0500

    ejercicio terminado

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var num1, num2, num3, suma, promedio;
 
 num1 = prompt("Por favor, ingresa el primer número");
 num2 = prompt("Por favor, ingresa el segundo número");
-num2 = prompt("Por favor, ingresa el tercer número");
+num3 = prompt("Por favor, ingresa el tercer número");
 
 suma = parseFloat(num1) + parseFloat(num2) + parseFloat(num3);
 
-promedio = suma/3;
+promedio = parseFloat(suma)/3;
 
 alert(promedio);
